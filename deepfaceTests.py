773f006c4424f2028e1471b0b871748d6e286feb
Pepijn <pvwpvw@protonmail.com>
2023-04-23 15:27:16 +0200

    draw function works

--- deepfaceTests.py
+++ deepfaceTests.py
@@ -7,24 +7,28 @@ input_folder = 'inps/'
 def analyze_pic(path):
     embeddings = DeepFace.represent(img_path = path)
 
-    print(len(embeddings))
-
     vec = embeddings[0]['embedding']
     face = embeddings[0]['facial_area']
 
-    return vec, face_area
+    return vec, face
 
 
-def draw_face_rect(img, face_area):
+def draw_face_rect(img_path, face_area):
+    img = cv2.imread(img_path)
     lx = face_area["x"]
     rx = lx + face_area["w"]
     ty = face_area["y"]
     by = ty + face_area["h"]
 
-    new_im = cv2.rectangle(img, (ty, lx), (by, rx), (255, 0, 0), 2)
-    cv2.imwrite("rect.png")
+    new_im = cv2.rectangle(img, (lx, ty), (rx, by), (255, 0, 0), 2)
+    cv2.imwrite("rect.png", new_im)
 
+
+print("Analyzing face...")
 v, fa = analyze_pic(f'{input_folder}pep1.JPG')
+print("drawing rectangle...")
+
+draw_face_rect(f'{input_folder}pep1.JPG', fa)
 # analyze_pic(f'{input_folder}pep_daanBril.JPG')
 
 
