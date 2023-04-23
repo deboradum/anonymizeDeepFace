4ee2a8c5de5b8bb8526727500d0b29348857dca3
Pepijn <pvwpvw@protonmail.com>
2023-04-23 15:14:34 +0200

    work on rectangle drawing

--- deepfaceTests.py
+++ deepfaceTests.py
@@ -1,5 +1,6 @@
 from deepface import DeepFace
 from pprint import pprint
+import cv2
 
 input_folder = 'inps/'
 
@@ -11,8 +12,22 @@ def analyze_pic(path):
     vec = embeddings[0]['embedding']
     face = embeddings[0]['facial_area']
 
-    return vec, face
+    return vec, face_area
 
 
-analyze_pic(f'{input_folder}pep1.JPG')
-analyze_pic(f'{input_folder}pep_daanBril.JPG')
+def draw_face_rect(img, face_area):
+    lx = face_area["x"]
+    rx = lx + face_area["w"]
+    ty = face_area["y"]
+    by = ty + face_area["h"]
+
+    new_im = cv2.rectangle(img, (ty, lx), (by, rx), (255, 0, 0), 2)
+    cv2.imwrite("rect.png")
+
+v, fa = analyze_pic(f'{input_folder}pep1.JPG')
+# analyze_pic(f'{input_folder}pep_daanBril.JPG')
+
+
+
+
+
